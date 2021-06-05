3348844a987bb6897ab92741b716c0a72a6c71ff
fazrulhz <ardin260@gmail.com>
2021-06-06 01:04:10 +0800

    Configurasi cors

--- app.js
+++ app.js
@@ -28,7 +28,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({ origin: '*' }));
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+}));
 
 app.use('/', indexRouter);
 app.use('/refdesa', refdesaRouter);
