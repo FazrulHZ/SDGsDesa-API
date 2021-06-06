15c97c34ae4b9f3df39ff670049cbdf980b46497
fazrulhz <ardin260@gmail.com>
2021-06-06 20:38:33 +0800

    Fixed cors

--- app.js
+++ app.js
@@ -30,14 +30,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors(
-  {
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  }
-));
+var corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}
+app.options('*', cors(corsOptions));
 
 app.use('/', indexRouter);
 app.use('/refdesa', refdesaRouter);
