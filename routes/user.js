565fbaddd83b36ed4b2ff496b83aaca712bc8767
fazrulhz <ardin260@gmail.com>
2021-09-08 23:23:04 +0800

    Route profil user

--- routes/user.js
+++ routes/user.js
@@ -22,6 +22,22 @@ var upload = multer({ storage: storage });
 
 router.get('/', auth, async function (req, res, next) {
 
+    const role = req.user;
+
+    const cekAuth = await new Promise(resolve => {
+        connection.query('SELECT * FROM tb_user WHERE user_id = ?', [role.id], function (error, rows, field) {
+            if (error) {
+                console.log(error)
+            } else {
+                resolve(rows[0]);
+            }
+        });
+    });
+
+    if (cekAuth.user_lvl !== '1') {
+        return response.noAkses(res);
+    }
+
     const count = await new Promise(resolve => {
         connection.query('SELECT COUNT(*) AS cnt FROM tb_user', function (error, rows, field) {
             if (error) {
@@ -39,20 +55,6 @@ router.get('/', auth, async function (req, res, next) {
             response.ok(true, 'Data Berhasil Diambil', count, rows, res);
         }
     });
-});
-
-router.get('/:id', auth, function (req, res, next) {
-
-    var user_id = req.params.id;
-
-    connection.query('SELECT * FROM tb_user WHERE user_id = ?', [user_id],
-        function (error, rows, field) {
-            if (error) {
-                console.log(error);
-            } else {
-                response.ok(true, 'Data Berhasil Diambil', 1, rows[0], res);
-            }
-        });
 });
 
 router.post('/', auth, upload.single('user_foto'), async function (req, res, next) {
@@ -238,6 +240,86 @@ router.delete('/:id', auth, async function (req, res) {
                 })
             }
         });
+    }
+});
+
+router.get('/profil', auth, async function (req, res, next) {
+
+    const role = req.user;
+
+    const cekAuth = await new Promise(resolve => {
+        connection.query('SELECT * FROM tb_user WHERE user_id = ?', [role.id], function (error, rows, field) {
+            if (error) {
+                console.log(error)
+            } else {
+                resolve(rows[0]);
+            }
+        });
+    });
+
+    connection.query('SELECT * FROM tb_user WHERE user_id = ?', [role.id], function (error, rows, field) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok(true, 'Data Berhasil Diambil', 1, rows[0], res);
+        }
+    });
+});
+
+router.put('/profil', auth, upload.single('desa_foto'), async function (req, res, next) {
+
+    let user_id = req.body.user_id;
+    let user_ktp = req.body.user_ktp;
+    let user_nama = req.body.user_nama;
+    let user_name = req.body.user_name;
+    let user_password = req.body.user_password;
+    let user_tlp = req.body.user_tlp;
+    let user_alamat = req.body.user_alamat;
+    let user_lvl = req.body.user_lvl;
+    let kabupaten_id = req.body.kabupaten_id;
+    let kecamatan_id = req.body.kecamatan_id;
+    let desa_id = req.body.desa_id;
+
+    const check = await new Promise(resolve => {
+        connection.query('SELECT COUNT(user_name) AS cnt, user_foto, user_id FROM tb_user WHERE user_name = ?', [user_name], function (error, rows, field) {
+            if (error) {
+                console.log(error)
+            } else {
+                resolve(rows[0]);
+            }
+        });
+    });
+
+    let user_foto = req.file === undefined ? check.user_foto : req.file.filename;
+
+    if (user_password == "") {
+        if (check.cnt > 0 && check.user_id != user_id) {
+            response.error(false, "Username Telah Terdaftar!", 'empty', res);
+        } else {
+            connection.query('UPDATE tb_user SET user_ktp=?, user_nama=?, user_name=?, user_tlp=?, user_alamat=?, user_foto=?, user_lvl=?, kabupaten_id=?, kecamatan_id=?, desa_id=? WHERE user_id=?', [user_ktp, user_nama, user_name, user_tlp, user_alamat, user_foto, user_lvl, kabupaten_id, kecamatan_id, desa_id, user_id], function (error, rows, field) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    response.ok(true, "Berhasil Merubah Data!", 0, 'success', res)
+                }
+            })
+        }
+    } else {
+        //Hash Password
+        var salt = bcrypt.genSaltSync(10);
+        var pass = bcrypt.hashSync('' + user_password + '', salt);
+
+        if (check.cnt > 0 && check.user_id != user_id) {
+            response.error(false, "Username Telah Terdaftar!", 'empty', res);
+        } else {
+            connection.query('UPDATE tb_user SET user_ktp=?, user_nama=?, user_name=?, user_password=?, user_tlp=?, user_alamat=?, user_foto=?, user_lvl=?, kabupaten_id=?, kecamatan_id=?, desa_id=? WHERE user_id=?', [user_ktp, user_nama, user_name, pass, user_tlp, user_alamat, user_foto, user_lvl, kabupaten_id, kecamatan_id, desa_id, user_id], function (error, rows, field) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    response.ok(true, "Berhasil Merubah Data!", 0, 'success', res)
+                }
+            })
+        }
     }
 });
 
